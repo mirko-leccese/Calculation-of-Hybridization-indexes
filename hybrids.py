e1e2379ddc8537096742dc27e8c1fb207e34643f
Mirko Leccese <leccese_mirko@yahoo.it>
2022-04-16 20:29:39 +0200

    add coronene.xyz and function read_xyz

--- hybrids.py
+++ hybrids.py
@@ -1,1 +1,32 @@
-#Python script that computes the hybridization of C atoms 
+'''This Python script reads the geometrical structure of a molecule, as given in the xyz file, and computes the hybridization indexes and pyramid
+angle of C atoms, if any'''
+
+# Defining function read_xyz() that reads a xyz file (filename)
+def read_xyz(filename):
+    '''This function return a list of atoms and coordinates
+    
+    If the number of coordinates does not agree with the number
+    of atoms within the xyz file, it raises a ValueError'''
+
+    atoms = []
+    coordinates = []
+
+    xyz_file = open(filename)
+    
+    # Reading number of atoms
+    n_atoms = int(xyz_file.readline())
+    title = xyz_file.readline()
+    for line in xyz_file:
+        atom, x, y, z = line.split()
+        atoms.append(atom)
+        coordinates.append([float(x),float(y), float(z)])
+    xyz_file.close()
+
+    if n_atoms != len(coordinates):
+        raise ValueError("File says %d atoms but read %d points." % (n_atoms, len(coordinates)))
+
+    return atoms, coordinates
+
+
+
+
